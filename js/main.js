fb85cf86057521c711ed6852a1097daf595face3
franci-xef <franciaurel07@gmail.com>
2024-12-04 23:02:17 +0100

    better graphic commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 function addCheck() {
-    
+    document.querySelector("#morning").addEventListener("click", addMorningCheck)
+    document.querySelector("#afternoon").addEventListener("click", addAfternoonCheck)
+    document.querySelector("#evening").addEventListener("click", addEveningCheck)
 }
 
-document.querySelector("#add_button").addEventListener("click", addCheck)
+function removeListener() {
+    document.querySelector("#morning").removeEventListener("click", addMorningCheck)
+    document.querySelector("#afternoon").removeEventListener("click", addAfternoonCheck)
+    document.querySelector("#evening").removeEventListener("click", addEveningCheck)
+}
+
+function addMorningCheck() {
+    removeListener()
+
+
+}
+
+function addAfternoonCheck() {
+    removeListener()
+
+
+}
+
+function addEveningCheck() {
+    removeListener()
+
+
+}
+
+document.querySelector("#add").addEventListener("click", addCheck);
