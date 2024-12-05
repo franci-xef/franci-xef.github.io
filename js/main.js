3ec4b41f6fbf1a10c027da19bc1612ff4891a4cf
franci-xef <franciaurel07@gmail.com>
2024-12-05 14:35:31 +0100

    animation commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
+const morningDiv = document.getElementById("morning")
+const afternoonDiv = document.getElementById("afternoon")
+const eveningDiv = document.getElementById("evening")
+
 function addCheck() {
     document.querySelector("#morning").addEventListener("click", addMorningCheck)
+    morningDiv.classList.add("animation")
+    
     document.querySelector("#afternoon").addEventListener("click", addAfternoonCheck)
+    afternoonDiv.classList.add("animation")
+
     document.querySelector("#evening").addEventListener("click", addEveningCheck)
+    eveningDiv.classList.add("animation")
 }
 
 function removeListener() {
@@ -10,21 +19,27 @@ function removeListener() {
     document.querySelector("#evening").removeEventListener("click", addEveningCheck)
 }
 
+function removeClass() {
+    morningDiv.classList.remove("animation")
+    afternoonDiv.classList.remove("animation")
+    eveningDiv.classList.remove("animation")
+}
+
 function addMorningCheck() {
     removeListener()
-
+    removeClass()
 
 }
 
 function addAfternoonCheck() {
     removeListener()
-
+    removeClass()
 
 }
 
 function addEveningCheck() {
     removeListener()
-
+    removeClass()
 
 }
 
